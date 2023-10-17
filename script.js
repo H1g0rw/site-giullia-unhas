4f194348141f63e435e39199e2f0c83a597a37df
H1g0rw <127334878+H1g0rw@users.noreply.github.com>
2023-10-17 07:53:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function abririnsta(){
+    window.open(`https://www.instagram.com/giullia.marks/`)
+}
+
+function abrirwpp(){
+    window.open(`https://wa.me/5511911368436?text=Ol%C3%A1%20gi,%20gostaria%20de%20marcar%20um%20hor%C3%A1rio`)
+}
